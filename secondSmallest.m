3edb828e41c9068547a882fc187dbafda5e3d94c
blueRoseXIV <32766153+blueRoseXIV@users.noreply.github.com>
2018-09-10 12:32:40 +0300

    Add files via upload

---
+++ secondSmallest.m
@@ -0,0 +1,20 @@
+function [temp2, p2] = secondSmallest (v)
+  n=length(v);
+  temp=realmax;
+  p=1;
+  for i=1:n
+    if v(i)<temp
+      temp=v(i);
+      p=i;
+    endif
+  endfor
+  v(p)=realmax;
+  temp2=realmax;
+  p2=1;
+  for i=1:n
+    if v(i)<temp2
+      temp2=v(i);
+      p2=i;
+    endif
+  endfor
+endfunction
