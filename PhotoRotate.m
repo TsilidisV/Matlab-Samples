1fca03b766a1997aa15ebfcf774c179208d5ee71
blueRoseXIV <32766153+blueRoseXIV@users.noreply.github.com>
2018-09-19 12:20:48 +0300

    Add files via upload
    Γράψτε συνάρτηση PhotoRotate(filename) που να δέχεται ένα ασπρόμαυρο jpeg αρχείο εικόνας filename, να περιστρέφει την εικόνα κατά 90◦ αριστερόστροφα και να εμφανίζει την περιστραμμένη εικόνα.

---
+++ PhotoRotate.m
@@ -0,0 +1,12 @@
+function PhotoRotate(filename)
+  P = imread(filename);
+  [n,m]=size(P);
+  rot=zeros(m,n);
+  rot = uint8(rot);
+  for i=0:n-1
+    for j=0:m-1
+      rot(m-j,i+1)=P(i+1,j+1);
+    endfor
+  endfor
+  imshow(rot)
+endfunction
